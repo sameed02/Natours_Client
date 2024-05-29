f389466610d614945220463a2b17012053146cc8
sameed02 <sameedahmad3@gmail.com>
2024-05-29 18:46:03 +0530

    updated hero section, 1. added logo, 2.added primary heading and secondary heading. i have also updated img names in lower camelCase

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 :root {
    
+      --color-white: #fff;
       --color-grey: #777;
 
       --color-light-green: #7ed56f;
@@ -28,19 +29,6 @@ const GlobalStyle = createGlobalStyle`
     padding: 30px;
   }
 
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'Georgia', serif;
-    margin-bottom: 1rem;
-  }
-
-  a {
-    color: #007bff;
-    text-decoration: none;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
 `;
 
 export default GlobalStyle;
--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import heroImage from "../assets/img/hero.jpg";
+import logoWhite from "/public/logoWhite.png";
 
 const StyledHero = styled.div`
+  position: relative;
   height: 95vh;
   background-image: linear-gradient(
       to right bottom,
@@ -19,8 +21,61 @@ const StyledHero = styled.div`
   ); // works clockwise (x y) axis
 `;
 
+const ImgContainer = styled.div`
+  position: absolute;
+  top: 40px;
+  left: 40px;
+`;
+
+const StyledImg = styled.img`
+  height: 35px;
+`;
+
+const HeroBox = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%; // this top and left are in relation to its parent because of absolute positions and we have set its parent element as position: relative
+  transform: translate(
+    -50%,
+    -50%
+  ); // this translate is in relation to the element itself.
+`;
+
+const HeroHeading = styled.h1`
+  color: var(--color-white);
+  text-transform: uppercase;
+`;
+
+const HeroHeadingPrimary = styled.span`
+  display: block;
+  font-size: 60px;
+  font-weight: 700;
+  letter-spacing: 35px;
+  margin-right: -35px; // letter-spacing property also adds that space AFTER the last letter, So, in hero box we're centering the box, but not the text, because it has an extra 35px to the right, thus being the text more to the left side than it should. so we are adding negative margin to center the text
+`;
+const HeroHeadingSub = styled.span`
+  display: block;
+  font-size: 20px;
+  font-weight: 400;
+  letter-spacing: 17.4px;
+  margin-right: -17.4px;
+`;
+
 function Hero() {
-  return <StyledHero>Header</StyledHero>;
+  return (
+    <StyledHero>
+      <ImgContainer>
+        <StyledImg src={`${logoWhite}`} alt="Logo" />
+      </ImgContainer>
+
+      <HeroBox>
+        <HeroHeading>
+          <HeroHeadingPrimary>Outdoors</HeroHeadingPrimary>
+          <HeroHeadingSub>is where life happens</HeroHeadingSub>
+        </HeroHeading>
+      </HeroBox>
+    </StyledHero>
+  );
 }
 
 export default Hero;
