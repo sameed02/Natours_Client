28c08dcaef93bd9a2fab31c5c98bc340461f6d9b
sameed02 <sameedahmad3@gmail.com>
2024-06-03 17:30:55 +0530

    setup react query and react query dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Tours from "./pages/Tours.jsx";
 import PageNotFound from "./ui/PageNotFound.jsx";
 import Hero from "./ui/Hero.jsx";
 import Settings from "./pages/Settings.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter([
   {
@@ -32,11 +34,20 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
