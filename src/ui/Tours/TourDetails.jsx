2b59350a6610c91ebb56c249e79d70fa67bfbb01
sameed02 <sameedahmad3@gmail.com>
2024-06-13 20:57:04 +0530

    updated tour details page ui

--- src/ui/Tours/TourDetails.jsx
+++ src/ui/Tours/TourDetails.jsx
@@ -5,12 +5,80 @@ import {
   TourHeadingDetail,
   TourImgDetail,
 } from "./TourImgDetail.jsx";
-import { HiOutlineClock, HiOutlineLocationMarker } from "react-icons/hi";
+import {
+  HiOutlineCalendar,
+  HiOutlineClock,
+  HiOutlineLocationMarker,
+  HiOutlineStar,
+  HiOutlineTrendingUp,
+  HiOutlineUser,
+} from "react-icons/hi";
 
 const TourDetailsBox = styled.div`
   padding: 2rem 0 0 2rem;
   margin: 0 -1rem;
   overflow-y: auto;
+`;
+
+const Tourdescription = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: -25rem;
+
+  & h2 {
+    font-size: 2.8rem;
+    font-weight: 700;
+    background-image: linear-gradient(to right, #7dd56f, #28b487);
+    background-clip: text;
+    color: transparent;
+    letter-spacing: 0.1rem;
+    line-height: 1.3;
+    margin-bottom: 3.5rem !important;
+    text-transform: uppercase;
+  }
+
+  & p:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+`;
+
+const TourOverview = styled.div`
+  padding: 25rem 8rem;
+  background-color: #f7f7f7;
+  flex-grow: 1;
+  flex-basis: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const OverviewStat = styled.div`
+  & div {
+    font-size: 2rem;
+    padding: 1rem 0;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  & div svg {
+    font-size: 2.5rem;
+    margin-right: 0.5rem;
+    color: var(--color-medium-green);
+  }
+
+  & div span {
+    margin-left: 1rem;
+  }
+`;
+
+const TourOverviewDetail = styled.div`
+  padding: 25rem 8rem;
+  flex-grow: 1;
+  flex-basis: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 function TourDetails() {
@@ -47,6 +115,49 @@ function TourDetails() {
           </div>
         </HeadingDescription>
       </TourImgDetail>
+
+      <Tourdescription>
+        <TourOverview>
+          <h2>Quick Facts</h2>
+          <OverviewStat>
+            <div>
+              <HiOutlineUser />
+              People <span>15</span>
+            </div>
+            <div>
+              <HiOutlineStar />
+              Rating <span>4.8/5</span>
+            </div>
+            <div>
+              <HiOutlineTrendingUp />
+              Difficulty <span>medium</span>
+            </div>
+            <div>
+              <HiOutlineCalendar />
+              Next Date <span>June 2021</span>
+            </div>
+          </OverviewStat>
+        </TourOverview>
+
+        <TourOverviewDetail>
+          <h2>About the Sea Explorer</h2>
+          <p>
+            Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+          <p>
+            Irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </TourOverviewDetail>
+      </Tourdescription>
     </TourDetailsBox>
   );
 }
